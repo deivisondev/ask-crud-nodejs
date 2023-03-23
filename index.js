6eec2e20768b56450ea636c9de037abd00fcad6a
Deivison <101577438+deivisondev@users.noreply.github.com>
2023-03-23 08:29:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index", { nome: "Kono Subarashii Sekai Ni Shukufuku Wo!" });
+  res.render("index", { nome: "" });
 });
 
 app.listen(3000, () => console.log("App rodando na porta 3000..."));
